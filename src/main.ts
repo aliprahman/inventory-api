818c5bcbd728e4bd93ad96e25c0e803279611ead
alip rahman hakim <aliprrahman@gmail.com>
2024-07-08 06:10:20 +0700

    response interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from '@interceptor/response.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configSerivce = app.get(ConfigService);
+
+  // setup
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   const port = configSerivce.get('app.port');
   await app.listen(port);
